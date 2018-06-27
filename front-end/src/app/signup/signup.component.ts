a71bbc11ebdb2f74c8e94985c0bf38f29fea052e
Galloa118 <pewallin0527@gmail.com>
2018-06-27 22:49:06 +0300

    add front-end for slack login page using Angular5

---
+++ front-end/src/app/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onFormSubmit(form) {
+    this.router.navigateByUrl('/');
+  }
+}
